796c90834a73a4718f0be2358493e224443621e7
taylor18915 <54372304+taylor18915@users.noreply.github.com>
2019-10-23 14:54:38 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Candy3.Data;
+
+namespace Candy3.Pages.Ord
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Candy3.Data.ApplicationDbContext _context;
+
+        public IndexModel(Candy3.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Orderscs> Orderscs { get;set; }
+
+        public async Task OnGetAsync()
+        {    var can= from m  in  _context.Candy select m;
+            can = can.Where(s => s.Ctype.Contains("hershey"));
+             
+        }
+    }
+}
